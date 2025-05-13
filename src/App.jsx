161f2870de92a2781de6e789427b1b3b7d4d9d23
DouweWiegersma <douwewiegersma17@hotmail.nl>
2025-05-13 11:39:42 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import './App.css';
-
+import axios from 'axios'
+import {useState} from "react";
 function App() {
+
+    const [countrys, setCountrys] = useState([])
+
+
+
+async function fetchData(){
+        try {
+            const countryData = await axios.get("https://restcountries.com/v3.1/all?fields=name,population,flags")
+            console.log(countryData)
+            setCountrys(countryData.data)
+        }
+        catch(e){
+            console.error(e)
+        }
+    }
+
+
 
     return (
         <>
-            Maak je applicatie hier!
+
+           <button className="countryButton" onClick={ () => fetchData()}>Click here!</button>
+            <ul>
+            {countrys.map((country) => {
+                return <li key="name">
+                    <img src={country.flags.png} alt="flag" className="flags"/>
+                {country.name.common}q
+                {country.population}
+                </li>
+            })}
+            </ul>
         </>
     )
 }
